05432f52eba50273754b43f1b00e609e721e4633
Paulo Salvatore <salvatoregames@gmail.com>
2021-05-05 10:47:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,19 @@ app.get('/hello', function (req, res) {
 
 const mensagens = ['Essa é a primeira mensagem!', 'Essa é a segunda mensagem!'];
 
+// CRUD (Create, Read, Update, Delete)
+
+// GET: READ ALL (exibir todos os registros)
 app.get('/mensagens', function (req, res) {
   res.send(mensagens);
 });
+
+// GET: READ SINGLE (exibir apenas um registro)
+
+// POST: CREATE (criar um registro)
+
+// PUT: UPDATE (editar um registro)
+
+// DELETE: DELETE (remover um registro)
 
 app.listen(3000);
